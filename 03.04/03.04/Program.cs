a8df87a161f2f3157e55e1963a4fb6c6cf140898
Alexandrov911 <sashok17062002@mail.ru>
2021-05-11 14:39:41 +0300

    03.05

---
+++ 03.04/03.04/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03._04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.46
+            Console.WriteLine("введите число a");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("введите число b");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("введите число c");
+            double c = Convert.ToDouble(Console.ReadLine());
+            double v = a * b * c;
+            double s = 2 * (a * b + a * c);
+            Console.WriteLine("Объём=" + v);
+            Console.WriteLine("Площадь бок поверхности=" +s);
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
---
+++ 03.04/2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2.19
+            Console.WriteLine("введите четырехзначное число");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i <= 4; i++)
+            {
+               sum = n + i;
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+            
+        }
+    }
+}
